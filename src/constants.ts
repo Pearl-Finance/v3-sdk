23f390bd67ea26e373f001eaa0e574f33d2504d7
dancingswann <elizabethswann@pearl.exchange>
2024-01-25 12:49:55 +0100

    PEARL-X: Update init hash

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const FACTORY_ADDRESS = '0xf2A5DD9ff3aA93807986DeAA9fD316471BD959Cd'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xDE6D6E393546fcF5f75a473088aE1eB701129C33'
+export const POOL_INIT_CODE_HASH = '0xc32ed61a35ac401654bdc05f40637b01ce2fe2a5446cc86df1d00ac7faae2bcc'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
